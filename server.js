d119d14ee3bc46999dc0602ccbf1c0c613a9eeb1
Justin Vititoe <jvit1985@yahoo.com>
2022-05-09 13:35:45 -0500

    updated get request order

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//get request to return index.html file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-//get request to return notes.html file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
 //get api request to read db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     res.json(existingNotes);
@@ -55,6 +45,15 @@ app.post('/api/notes', (req, res) => {
     
 //Delete api notes id, should receive a query parameter containing the id of a note to delete. In order to delete a note you'll need to read all notes from the db.json file, remove the note with the given id property and then rewrite the notes to the db.json file
 
+//get request to return notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+//get request to return index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
