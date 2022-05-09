ff3d9fbc15cd6360ef0bdbe718cb64ba95341ba6
Justin Vititoe <jvit1985@yahoo.com>
2022-05-09 13:30:53 -0500

    updated post function

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const {v4 : uuidv4} = require('uuid');
 const path = require('path');
 const fs = require('fs');
 
-const existingNotes = require('./Develop/db/db.json');
-let notes = [];
-
-if(Array.isArray(existingNotes)) {
-    notes = [...existingNotes];
-} else {
-    notes = [existingNotes];
-}
+const existingNotes = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,20 +12,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// //get request to return notes.html file
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-// });
+//get request to return index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-// //get request to return index.html file
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-// });
+//get request to return notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 //get api request to read db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
+    res.json(existingNotes);
 });
 
 //post api request that receives a new note to save on the request body, add it to the db.json file and then return the new note to the client. Needs a unique id when it's saved.
@@ -48,40 +40,21 @@ app.post('/api/notes', (req, res) => {
         noteId,
     };
 
-    notes = [...notes, newNote];
-    const noteString = JSON.stringify(notes, null, 2);
+    let filePath = path.join(__dirname, "/db/db.json");
+    existingNotes.push(newNote);
 
-    fs.writeFile(`./db/db.json`, noteString, (err) => 
-        err
-            ? console.error(err)
-            : console.log(
-                `Note for ${newNote.title} has been written to JSON file`
-            )
-    );
-
-    const response = {
-        status: 'success',
-        body: newNote,
-    };
-
-    console.log(response);
-    res.json(response);
-    } else {
-        res.json('Error in posting note');
+    fs.writeFile(filePath, JSON.stringify(existingNotes), function (err) {
+        if(err) {
+            console.log(err);
+        }
+        console.log('Note has been added!');
+    });
     }
+    res.json(newNote);
 });
     
 //Delete api notes id, should receive a query parameter containing the id of a note to delete. In order to delete a note you'll need to read all notes from the db.json file, remove the note with the given id property and then rewrite the notes to the db.json file
 
-//get request to return notes.html file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-//get request to return index.html file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
